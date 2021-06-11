e35afcabc01e51eec051581df723d121a2227ac5
menjuban <54066941+menjuban@users.noreply.github.com>
2021-06-11 16:08:27 +0200

    add tests configuration

---
+++ __tests__/Login.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from '../client/components/Login'
+import {render} from '@testing-library/react'
+// import '@testing-library/jest-dom/extend-expect'
+
+describe('Login Component test',()=>{
+    test('Render Login component',()=>{
+        render(<Login />)
+    
+    
+    })
+})    
